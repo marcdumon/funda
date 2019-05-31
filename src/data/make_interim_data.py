ff64f13f5873ef1493a533e0a6508629d9f9c2cf
marcdumon <marcdumon@msn.com>
2019-06-01 01:07:25 +0200

    Continue make_interim_data.py
    - Finish make_interim_inflation()
    - Initial adjust_inflation()
    - Initial make_labels()

--- src/data/make_interim_data.py
+++ src/data/make_interim_data.py
@@ -29,6 +29,7 @@ raw_stockpup_path = 'raw/stockpup/'
 raw_yahoo_quotes_path = 'raw/yahoo_quotes/'
 raw_yahoo_info_path = 'raw/yahoo_info/'
 raw_edgar_path = 'raw/edgar/'
+raw_economic_indicators = 'raw/economic_indicators/'
 
 interim_data_path = 'interim/'
 
@@ -44,34 +45,43 @@ def make_tickers():
     indexes.to_csv(base_data_path + interim_data_path + 'indexes.csv')
 
 
+def make_interim_inflation():
+    inflation = pd.read_csv(base_data_path + raw_economic_indicators + 'monthly_inflation_rates.csv')
+    inflation['CPI_inverse'] = (1 + inflation['Value'].iloc[::-1].values / 100) ** (1 / 12)
+    inflation['CPI_multiplier'] = inflation['CPI_inverse'].cumprod().iloc[::-1].values
+    inflation['20190404_multiplier'] = inflation['CPI_multiplier']  # .iloc[::-1].values
+    inflation['Date'] = pd.to_datetime(inflation['TIME'], format='%Y-%m')
+    inflation = inflation[['Date', 'Value', '20190404_multiplier']]
+    inflation.to_csv(base_data_path + interim_data_path + 'economic_indicators/monthly_inflation_rates.csv')
+
+
 def make_interim_stockpup():
     stockpup = pd.read_csv(base_data_path + raw_stockpup_path + 'stockpup_raw_data.csv', index_col=0)
-    # ticker_info = pd.read_csv(base_data_path + raw_yahoo_info_path + 'yahoo_info_raw_data.csv', index_col=0)
-
     # Make dates
     stockpup['Quarter end'] = pd.to_datetime(stockpup['Quarter end'], format='%Y-%m-%d')
     stockpup['Quarter start'] = stockpup['Quarter end'] - pd.DateOffset(months=3) + pd.offsets.MonthBegin(1)
 
     # CLEAN-UP
     # Remove and reorder columns
-    cols = ['Ticker', 'Quarter start', 'Quarter end', 'Shares', 'Shares split adjusted', 'Split factor',
-            # Balance sheet statement
-            'Assets', 'Current Assets', 'Liabilities', 'Current Liabilities', 'Shareholders equity',
-            'Non-controlling interest', 'Preferred equity', 'Goodwill & intangibles', 'Long-term debt',
-            # Income statement
-            'Revenue', 'Earnings', 'Earnings available for common stockholders',
-            'EPS basic', 'EPS diluted', 'Dividend per share',
-            # Cashflow statement
-            'Cash from operating activities', 'Cash from investing activities', 'Cash from financing activities',
-            'Cash change during period', 'Cash at end of period', 'Capital expenditures',
-            # Ratio's
-            'ROE', 'ROA', 'Book value of equity per share', 'P/B ratio', 'P/E ratio',
-            'Dividend payout ratio', 'Long-term debt to equity ratio',
-            'Equity to assets ratio', 'Net margin', 'Asset turnover', 'Free cash flow per share', 'Current ratio',
-            # Removed columns
-            # 'Cumulative dividends per share', #  Dividents from 1st reporting quarter until now. Irrelevant
-            # 'Price', 'Price high', 'Price low', # Will get prices from yahoo_quotes
-            ]
+    col_info = ['Ticker', 'Quarter start', 'Quarter end', 'Shares', 'Shares split adjusted', 'Split factor']
+    # Balance sheet statement
+    col_balance = ['Assets', 'Current Assets', 'Liabilities', 'Current Liabilities', 'Shareholders equity',
+                   'Non-controlling interest', 'Preferred equity', 'Goodwill & intangibles', 'Long-term debt']
+    # Income statement
+    col_income = ['Revenue', 'Earnings', 'Earnings available for common stockholders',
+                  'EPS basic', 'EPS diluted', 'Dividend per share']
+    # Cashflow statement
+    col_cf = ['Cash from operating activities', 'Cash from investing activities', 'Cash from financing activities',
+              'Cash change during period', 'Cash at end of period', 'Capital expenditures']
+    # Ratio's
+    col_ratio = ['ROE', 'ROA', 'Book value of equity per share', 'P/B ratio', 'P/E ratio',
+                 'Dividend payout ratio', 'Long-term debt to equity ratio',
+                 'Equity to assets ratio', 'Net margin', 'Asset turnover', 'Free cash flow per share', 'Current ratio']
+    # Removed columns
+    # 'Cumulative dividends per share', #  Dividents from 1st reporting quarter until now. Irrelevant
+    # 'Price', 'Price high', 'Price low', # Will get prices from yahoo_quotes
+
+    cols = col_info + col_income + col_cf + col_ratio
     stockpup = stockpup[cols]
     print(stockpup.sample(10))
     stockpup.to_csv(base_data_path + interim_data_path + 'stockpup_interim_data.csv')
@@ -158,7 +168,7 @@ def make_interim_data(start_from='A', end_till='ZZZZZ', write_log=False):
     for ticker in tickers['Ticker'].values:
         if start_from > ticker: continue
         if ticker > end_till: continue
-        blacklist=[]
+        blacklist = []
         ticker_interim_data = pd.DataFrame()
         print(ticker)
         try:
@@ -176,7 +186,7 @@ def make_interim_data(start_from='A', end_till='ZZZZZ', write_log=False):
             mask = (ticker_edgar['Quarter end'] == row['Quarter end']) & ((ticker_edgar['Type'] == '10-K') | (ticker_edgar['Type'] == '10-Q'))
             try:
                 filing_date = ticker_edgar.loc[mask, 'Filing date'].values[0]
-                print(ticker,filing_date)
+                print(ticker, filing_date)
             except IndexError:
                 print('Filing date does\'t exist:' + ',' + row['Quarter end'])
                 continue
@@ -277,13 +287,48 @@ def make_interim_data(start_from='A', end_till='ZZZZZ', write_log=False):
         print(ticker_interim_data.head(15))
         print('-' * 200)
 
-    print('#'*120)
+    print('#' * 120)
     print(blacklist)
-# 1/0
 
 
-def make_labels():
-    pass
+def adjust_inflation(df: DataFrame) -> DataFrame:
+    # Todo: Implement adjust_inflation
+    ticker = 'AAPL'
+    ticker_interim_data = pd.read_csv(base_data_path + interim_data_path + 'interim_data/' + ticker + '.csv', index_col=0)
+    inflation = pd.read_csv(base_data_path + interim_data_path + 'economic_indicators/monthly_inflation_rates.csv', index_col=0)
+    print(type(inflation['Date'][0]))
+    ticker_interim_data['tmp_left_on'] = ticker_interim_data['Filing date'].str.rsplit('-', n=1, expand=True)[0]  # yyyy-mm
+    inflation['tmp_right_on'] = inflation['Date'].str.rsplit('-', n=1, expand=True)[0]  # yyyy-mm
+    # merge dataframe
+    ticker_interim_data = pd.merge(ticker_interim_data, inflation, how='left', left_on='tmp_left_on', right_on='tmp_right_on')
+    # Clean up
+    ticker_interim_data.rename(index=str, columns={'Value': 'Inflation'}, inplace=True)
+    ticker_interim_data.drop(['tmp_left_on', 'tmp_right_on', 'Date'], axis=1, inplace=True)
+    print(ticker_interim_data)
+
+
+def make_labels(fwd_quarters=5):
+    tickers = pd.read_csv(base_data_path + 'interim/tickers.csv', index_col=0)
+    df = pd.DataFrame()
+    for ticker in tickers['Ticker'].values:
+        # if ticker>'B': continue
+        print(ticker)
+        # print(ticker_interim_data.loc[ticker_interim_data['Close'].pct_change(periods=-fwd_quarters)>0.1,'Close'])
+        # print(ticker_interim_data.loc[ticker_interim_data['^DJI Close'].pct_change(periods=-fwd_quarters)>0.1,['Filing date', '^DJI Close']])
+        try:
+            ticker_interim_data = pd.read_csv(base_data_path + interim_data_path + 'interim_data/' + ticker + '.csv', index_col=0)
+            x = ticker_interim_data['Close'].pct_change(periods=fwd_quarters).shift(-fwd_quarters)
+            dji = ticker_interim_data['^DJI Close'].pct_change(periods=fwd_quarters).shift(-fwd_quarters)
+            sp500 = ticker_interim_data['^GSPC Close'].pct_change(periods=fwd_quarters).shift(-fwd_quarters)
+            nasdaq = ticker_interim_data['^IXIC Close'].pct_change(periods=fwd_quarters).shift(-fwd_quarters)
+            ref = (dji + sp500 + nasdaq) / 3
+            df = pd.concat([df, x-ref], axis=0)
+            df.dropna(inplace=True)
+        except (KeyError,FileNotFoundError):
+            print('------------------->',ticker)
+    df.to_csv('xxx.csv')
+    df.plot.hist(bins=100)
+    plt.show()
 
 
 def combine_interim_data():
@@ -292,6 +337,7 @@ def combine_interim_data():
 
 if __name__ == '__main__':
     # make_tickers()
+    # make_interim_inflation()
     # make_interim_yahoo_quotes()
     # make_interim_yahoo_info()
     # make_interim_stockpup()
@@ -301,7 +347,7 @@ if __name__ == '__main__':
     # make_interim_data(start_from='I',end_till='M', write_log=False)
     # make_interim_data(start_from='M',end_till='Q', write_log=False) *
     # make_interim_data(start_from='Q',end_till='U', write_log=False)
-    make_interim_data(start_from='U',end_till='Z', write_log=False)
-
-    # make_interim_data(start_from='MLM',end_till='Q', write_log=False)
     # make_interim_data(start_from='U',end_till='Z', write_log=False)
+    make_labels()
+    #
+    # adjust_inflation()
